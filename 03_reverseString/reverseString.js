26e99b8196cad69de9ae9cff7f74f5e0d6ee0809
Devon <dev.kahika.campbell@gmail.com>
2022-04-28 05:42:43 +1200

    Complete a bunch of challenges

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const reversedList = []
+      for (char of string) {
+          reversedList.unshift(char);
+      }
+    let reversedString = reversedList.join("");  
+    return reversedString;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
-};
+const sumAll = function(num1, num2) {
+    // list to contain numbers between parameters
+    const listOfNum = []
+    debugger;
+    // i will become first number and add to list, add 1, add to list, repeat until num1 = num2
+    for (let i = num1; i === num2; i++) {
+        listOfNum.push(i);
+        console.log(listOfNum)
+    }
+    // add all elements in list
+    const sumOfList = listOfNum.reduce(function(total, currentValue) {
+        return total + currentValue;
+    }
+    )
+    return sumOfList;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
